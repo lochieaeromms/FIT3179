492078bb4bd90998478870ad8d95757c4bed064e
lochieaeromms <64345549+lochieaeromms@users.noreply.github.com>
2023-10-07 15:28:08 +1100

    Add files via upload

---
+++ Week10/a2w10.js
@@ -0,0 +1,9 @@
+var vg_1 = "ChoroplethW10.vg.json";
+vegaEmbed("#vis1", vg_1,{'actions':false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "stackedBarW10.vg.json";
+vegaEmbed("#vis2", vg_2,{'actions':false}).then(function(result) {
+
+}).catch(console.error);
