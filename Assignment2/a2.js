aaf8053fc610ef61ca109316fca55f875f9f36c0
lochieaeromms <64345549+lochieaeromms@users.noreply.github.com>
2023-10-11 01:52:31 +1100

    Add files via upload

---
+++ Assignment2/a2.js
@@ -0,0 +1,27 @@
+var vg_1 = "Choroplethv2.vg.json";
+vegaEmbed("#vis1", vg_1,{'actions':false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "stackedBar.vg.json";
+vegaEmbed("#vis2", vg_2,{'actions':false}).then(function(result) {
+
+}).catch(console.error);
+var vg_3 = "DotMap.vg.json";
+vegaEmbed("#vis3", vg_3,{'actions':false}).then(function(result) {
+
+}).catch(console.error);
+var vg_4 = "line.vg.json";
+vegaEmbed("#vis4", vg_4,{'actions':false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_5 = "density.vg.json";
+vegaEmbed("#vis5", vg_5,{'actions':false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_6 = "gantt.vg.json";
+vegaEmbed("#vis6", vg_6,{'actions':false}).then(function(result) {
+
+}).catch(console.error);
